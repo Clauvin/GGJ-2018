1eec0c1220bff019e4876d90ea90924d2025dada
Clauvin <clauvin_almeida@hotmail.com>
2018-01-28 11:12:27 -0200

    TUTORIAL quase pronto

--- Love Transmission/Assets/Scripts/Util/MenuActions.cs
+++ Love Transmission/Assets/Scripts/Util/MenuActions.cs
@@ -6,8 +6,8 @@ public class MenuActions : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        ItsNotHereAnymore(GameObject.Find("Credits"));
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -26,11 +26,34 @@ public class MenuActions : MonoBehaviour {
 
     public void CreditsButtonAction()
     {
+        ItsNotHereAnymore(GameObject.Find("Menu, Parte Principal"));
+        ItsHereLook(GameObject.Find("Credits"));
+    }
 
+    public void BackCreditsButtonAction()
+    {
+        ItsNotHereAnymore(GameObject.Find("Credits"));
+        ItsHereLook(GameObject.Find("Menu, Parte Principal"));
     }
 
     public void CloseButtonAction()
     {
         Closed_Basics_3.LoadScene.CloseGame();
+    }
+
+    private void ItsNotHereAnymore(GameObject gc)
+    {
+        Vector3 pos_original = gc.transform.position;
+        Vector3 pos_nova = pos_original;
+        pos_nova.x += 100000;
+        gc.transform.position = pos_nova;
+    }
+
+    private void ItsHereLook(GameObject gc)
+    {
+        Vector3 pos_original = gc.transform.position;
+        Vector3 pos_nova = pos_original;
+        pos_nova.x -= 100000;
+        gc.transform.position = pos_nova;
     }
 }
