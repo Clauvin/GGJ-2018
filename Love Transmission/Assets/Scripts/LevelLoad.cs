4f4f66e3b7beabdda88859cfaad0543284fddbb1
eidercarlos <eidercarlos@gmail.com>
2018-01-27 22:18:32 -0200

    Movimentação, Câmera e Tiros funcionando.

---
+++ Love Transmission/Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour {
+
+    public void loadLevel()
+    {
+        //Load the level from LevelToLoad
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
