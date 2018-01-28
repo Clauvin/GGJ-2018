c188ad2645a287e483b65aa33169b583ff91ef81
Clauvin <clauvin_almeida@hotmail.com>
2018-01-28 10:12:59 -0200

    No dia anterior...

---
+++ Love Transmission/Assets/Scripts/Util/MenuActions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuActions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayButtonAction()
+    {
+        Closed_Basics_3.LoadScene.Load(1);
+    }
+
+    public void SettingsButtonAction()
+    {
+
+    }
+
+    public void CreditsButtonAction()
+    {
+
+    }
+
+    public void CloseButtonAction()
+    {
+        Closed_Basics_3.LoadScene.CloseGame();
+    }
+}
